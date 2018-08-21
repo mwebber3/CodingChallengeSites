5c796dc21ba09f35bf3b0ab9db1d092415301fac
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-21 11:10:55 -0500

    Added completed challenge.

---
+++ Kattis/Python/AlphabetSpam.py
@@ -0,0 +1,21 @@
+sentence = raw_input()
+length = len(sentence)
+whitespace = 0.0
+lowercase = 0.0
+uppercase = 0.0
+symbols = 0.0
+
+for i in sentence:
+    if i == "_":
+        whitespace += 1
+    elif i.islower():
+        lowercase += 1
+    elif i.isupper():
+        uppercase += 1
+    else:
+        symbols += 1
+
+print("%0.16f" % (whitespace / length))
+print("%0.16f" % (lowercase / length))
+print("%0.16f" % (uppercase / length))
+print("%0.16f" % (symbols / length))
