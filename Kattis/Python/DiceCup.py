01775339a3af2e5d2700c21e640622e9d5ca498b
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-21 00:58:50 -0500

    Added completed challenge.

---
+++ Kattis/Python/DiceCup.py
@@ -0,0 +1,12 @@
+firstRoll = input()
+secondRoll = input()
+lowestRoll = firstRoll
+highestRoll = secondRoll
+
+if (secondRoll < firstRoll):
+    lowestRoll = secondRoll
+    highestRoll = firstRoll
+
+for i in range ((lowestRoll + 1), (highestRoll + 2)):
+    print i
+    i = i + 1
