2ee671a2876d9f165b7019c43891e8e549f7e68a
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-21 21:02:08 -0500

    Added completed challenge.

---
+++ Kattis/CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Planina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, points;
+            n = Convert.ToInt32(Console.ReadLine());
+            points = (int) Math.Pow((Math.Pow(2, n) + 1), 2);
+            Console.WriteLine(points);
+            // for testing: Console.ReadKey();
+        }
+    }
+}
