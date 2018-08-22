f6911b36a0aef16e802b85f294d980d2d19d712a
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-22 01:19:47 -0500

    Added completed challenge.

---
+++ Kattis/CSharp/DiceCup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DiceCup
+{
+    class DiceCup
+    {
+        static void Main(string[] args)
+        {
+            int i, firstRoll, secondRoll, lowestRoll, highestRoll;
+            firstRoll = Convert.ToInt32(Console.ReadLine());
+            secondRoll = Convert.ToInt32(Console.ReadLine());
+            lowestRoll = firstRoll;
+            highestRoll = secondRoll;
+
+            if (secondRoll < firstRoll)
+            {
+                lowestRoll = secondRoll;
+                highestRoll = firstRoll;
+            }
+
+            for (i = (lowestRoll + 1); i <= (highestRoll + 1); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
