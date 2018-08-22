475bf5f77a6abc9f6f06f315ad3173b423e7f08f
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-22 01:09:50 -0500

    Added completed challenge.

---
+++ Kattis/Beavergnaw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Beavergnaw
+{
+    class Beavergnaw
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int d = Convert.ToInt32(Console.ReadLine());
+                int v = Convert.ToInt32(Console.ReadLine());
+
+                if (d == 0 && v == 0)
+                {
+                    System.Environment.Exit(1);
+                }
+
+                double result = Math.Pow((Math.Pow(d, 3) * Math.PI / 6 - v) / (Math.PI / 6), (1.0 / 3));
+                Console.WriteLine("{0:N9}", result);
+            }
+        }
+    }
+}
