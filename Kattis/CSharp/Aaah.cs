8669f1107aa8b177d532846d2e8eefb9067ab6ac
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-21 22:32:10 -0500

    Added completed challenge.

---
+++ Kattis/CSharp/Aaah.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aaah_
+{
+    class Aaah
+    {
+        static void Main(string[] args)
+        {
+            string jonsAaah = Console.ReadLine();
+            string docsAaah = Console.ReadLine();
+
+            if (jonsAaah.Length >= docsAaah.Length)
+                Console.WriteLine("go");
+            else 
+                Console.WriteLine("no");
+        }
+    }
+}
