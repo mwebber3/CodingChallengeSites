7817c209c0ad8d811fb0434e3c4e8b12edd95238
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-25 13:25:04 -0500

    Added completed challenge.

---
+++ Kattis/CSharp/Kornislav.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Kornislav
+{
+    class Kornislav
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[4];
+
+            var values = Console.ReadLine().Split(' ').Select(token => int.Parse(token));
+            numbers = values.ToArray();
+
+            Array.Sort(numbers);
+            // Testing: foreach (int i in numbers) Console.WriteLine(i);
+            Console.WriteLine(numbers[0] * numbers[2]);
+        }
+    }
+}
