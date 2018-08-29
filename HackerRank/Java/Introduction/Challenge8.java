b2d1f675c65af2c0eecb2250bc6e6abb63597fb2
mwebber3 <mwebber3@users.noreply.github.com>
2018-08-28 22:22:53 -0500

    Added completed challenge.

---
+++ HackerRank/Java/Introduction/Challenge8.java
@@ -0,0 +1,34 @@
+// Java Datatypes
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+        int cases=sc.nextInt();
+
+        for(int i=0;i<cases;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>=Math.pow(-2, 31) && x<=(Math.pow(2, 31)-1))System.out.println("* int");
+                if(x>=Math.pow(-2, 63) && x<=(Math.pow(2, 63)-1))System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
+
+
