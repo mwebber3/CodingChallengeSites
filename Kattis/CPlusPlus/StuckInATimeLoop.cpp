f9ca7b5eeecabba31d8db9f625826e71e1b4b949
Kay Kay <mwebber3@users.noreply.github.com>
2020-01-27 15:56:29 -0500

    Added completed challenge.

---
+++ Kattis/CPlusPlus/StuckInATimeLoop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int amount;
+    std::cin >> amount;
+
+    for (int i = 1; i <= amount; i++) {
+        std::cout << i << " Abracadabra" << std::endl;
+    }
+}
