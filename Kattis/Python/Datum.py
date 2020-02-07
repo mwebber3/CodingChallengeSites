256bf58098573ad2703a89cffdc5ef44c97a9b29
Kay Kay <mwebber3@users.noreply.github.com>
2020-02-07 14:54:29 -0500

    Added completed challenge.

---
+++ Kattis/Python/Datum.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+day, month = [int(s) for s in input().split()]
+date = date(2009, month, day)
+
+print(days[date.weekday()])
