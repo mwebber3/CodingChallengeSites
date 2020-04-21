db502dd31bf49630d8565379a883c2a08a3bd7f0
Kay Kay <mwebber3@users.noreply.github.com>
2020-04-21 18:54:56 -0400

    Added completed challenge.

---
+++ Kattis/CPlusPlus/NumberFun.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    int testCaseAmount;
+    double a, b, c;
+    std::cin >> testCaseAmount;
+
+    for (int i = 0; i < testCaseAmount; i++) {
+        std::cin >> a >> b >> c;
+
+        if ((a + b) == c) {
+            std::cout << "Possible" << std::endl;
+        } else if ((a - b) == c || (b - a) == c) {
+            std::cout << "Possible" << std::endl;
+        } else if ((a / b) == c || (b / a) == c) {
+            std::cout << "Possible" << std::endl;
+        } else if ((a * b) == c) {
+            std::cout << "Possible" << std::endl;
+        } else {
+            std::cout << "Impossible" << std::endl;
+        }
+    }
+
+    return 0;
+}
