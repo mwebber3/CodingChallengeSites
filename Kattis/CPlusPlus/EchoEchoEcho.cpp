917ae005489cc32dabd550b8f3051027244a50a7
Kay Kay <mwebber3@users.noreply.github.com>
2021-06-28 14:03:21 -0500

    Added completed challenge.

---
+++ Kattis/CPlusPlus/EchoEchoEcho.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::string word;
+    std::cin >> word;
+    std::cout << word << " " << word << " " << word << std::endl;
+
+    exit(EXIT_SUCCESS);
+    std::cin.get();
+}
