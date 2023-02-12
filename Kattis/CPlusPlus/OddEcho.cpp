25bfce8f1f19587b4050a4cfcef473106f50a968
Kay Kay <mwebber3@users.noreply.github.com>
2023-02-12 16:01:21 -0600

    Added completed challenge.

--- Kattis/CPlusPlus/OddEcho.cpp
+++ Kattis/CPlusPlus/OddEcho.cpp
@@ -1,7 +1,28 @@
-#include <iostream> 
+#include <iostream>
+
+bool checkInput(int);
 
 int main() {
-  
-  exit(EXIT_SUCCESS);
-  std::cin.get();
+    int i, j, numOfLines;
+    std::cin >> numOfLines;
+    std::string words[numOfLines];
+    
+    if (checkInput(numOfLines) == false) {
+        std::cout << "Please re-enter value." << std::endl;
+    } else {
+        for (i = 0; i < numOfLines; i++) {
+            std::cin >> words[i];
+        }
+        for (j = 0; j <= numOfLines; j = (j + 2)) {
+            std::cout << words[j] << std::endl;
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+    std::cin.get();
+}
+
+bool checkInput(int numOfLines) {
+    bool result = (numOfLines < 1 || numOfLines > 10) ? false : true;
+    return result;
 }
