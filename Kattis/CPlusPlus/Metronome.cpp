2ce32cf5ffc20aedf7875e1d020cfd058298b1f2
Kay Kay <mwebber3@my.apsu.edu>
2023-06-04 00:24:10 -0500

    Added completed challenge.

---
+++ Kattis/CPlusPlus/Metronome.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    double rotations, input;
+    std::cin >> input;
+    rotations = (input / 4.0);
+    std::cout << rotations << std::endl;
+
+    exit(EXIT_SUCCESS);
+    std::cin.get();
+}
