e023e987792d4e2dd698fed8cfa913583b8f39a4
Kay Kay <mwebber3@my.apsu.edu>
2023-06-06 06:13:49 -0500

    Added completed challenge.

---
+++ Kattis/Python/CountTheVowels.py
@@ -0,0 +1,16 @@
+input = input().lower()
+vowels = 0
+
+for i in input:
+    if i == "a":
+        vowels += 1
+    elif i == "e":
+        vowels += 1
+    elif i == "i":
+        vowels += 1 
+    elif i == "o":
+        vowels += 1 
+    elif i == "u":
+        vowels += 1  
+
+print(vowels)
